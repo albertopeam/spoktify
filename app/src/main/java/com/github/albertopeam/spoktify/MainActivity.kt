66156a083eaae092f9a2c32bc955917ae83c6620
Alberto Penas Amor <alberto__pa@hotmail.com>
2020-09-11 22:02:10 +0200

    created project

---
+++ app/src/main/java/com/github/albertopeam/spoktify/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.github.albertopeam.spoktify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.albertopeam.spoktify.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/github/albertopeam/spoktify/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.github.albertopeam.spoktify.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
